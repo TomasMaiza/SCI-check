9b1f77027d7919432f20a57c2f68e267844185d1
tomasmaiza <tomimaiza@gmail.com>
2026-07-08 11:26:47 -0300

    Agrego librería de predicados indirectos

--- coverage_checker/predicates_2d.py
+++ coverage_checker/predicates_2d.py
@@ -26,25 +26,8 @@ class Predicates2d(AbstractPredicates):
     return ret
 
   def orient_LPI(self, r: Point2D, s: Point2D, f1: Halfspace2D, f2: Halfspace2D) -> OrientResult: # retorna IN, OUT, ON
-    t, u = f1.get_points()
-    a, b = f2.get_points()
-
-    beta = vectors_cross_product(s.x, s.y, r.x, r.y, t.x, t.y, u.x, u.y)
-
-    if beta == 0:
-      return OUT
-
-    alpha = vectors_cross_product(t.x, t.y, r.x, r.y, t.x, t.y, u.x, u.y)
-
-    O = beta * vectors_cross_product(a.x, a.y, b.x, b.y, a.x, a.y, r.x, r.y) 
-    + alpha * vectors_cross_product(a.x, a.y, b.x, b.y, r.x, r.y, s.x, s.y)
-
-    if beta < 0:
-      O = -O
-
-    if O > 0:
-      return IN
-    return OUT
+    # llamo al orient2d de la librería externa
+    pass
 
   def orient_TPI(self) -> OrientResult: # retorna IN, OUT, ON
     pass
