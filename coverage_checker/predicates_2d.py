7ba47719f7573080c02c6fe6304819f946d7cc20
tomasmaiza <tomimaiza@gmail.com>
2026-07-08 12:00:20 -0300

    Modifico implementación 2d de orient_lpi
    Directamente llamo a la función de la librería de Attene que calcula la orientación del punto implícito

--- coverage_checker/predicates_2d.py
+++ coverage_checker/predicates_2d.py
@@ -27,8 +27,31 @@ class Predicates2d(AbstractPredicates):
     return ret
 
   def orient_LPI(self, r: Point2D, s: Point2D, f1: Halfspace2D, f2: Halfspace2D) -> OrientResult: # retorna IN, OUT, ON
-    # llamo al orient2d de la librería externa
-    pass
+    t, u = f1.get_points()
+    a, b = f2.get_points()
+    # queremos calcular la orientación de f1 \cap rs respecto a f2
+    
+    rExp = pyattene.ExplicitPoint2D(r.x, r.y)
+    sExp = pyattene.ExplicitPoint2D(s.x, s.y)
+
+    tExp = pyattene.ExplicitPoint2D(t.x, t.y)
+    uExp = pyattene.ExplicitPoint2D(u.x, u.y)
+
+    aExp = pyattene.ExplicitPoint2D(a.x, a.y)
+    bExp = pyattene.ExplicitPoint2D(b.x, b.y)
+
+    # Punto implícito: intersección de rs con tu
+    pImp = pyattene.ImplicitPoint2D_SSI(rExp, sExp, tExp, uExp)
+
+    ori = pyattene.orient2d_IEE(pImp, aExp, bExp)
+
+    if ori == -1:
+      ret = OrientResult.OUT
+    elif ori == 0:
+      ret = OrientResult.ON
+    else:
+      ret = OrientResult.IN
+    return ret
 
   def orient_TPI(self) -> OrientResult: # retorna IN, OUT, ON
     pass
