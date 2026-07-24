8d900cbd9b86dfcf285f750017c27ff937cb5c02
tomasmaiza <tomimaiza@gmail.com>
2026-07-24 10:21:44 -0300

    Completo interfaz con entradas y salidas de los métodos

--- src/subregions/approximations/strategy.py
+++ src/subregions/approximations/strategy.py
@@ -1,16 +1,20 @@
 from abc import ABC, abstractmethod
 import numpy as np
+import numpy.typing as npt
+from affine_system import *
 
 class ApproximationStrategy(ABC):
   @abstractmethod
-  def __init__(self):
+  def __init__(self, subsystem: AffineMode):
     # Inicializa la función de aproximación y la función de error
     pass
 
-  def apply_approx(self):
+  @abstractmethod
+  def apply_approx(self, s: float, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     # aplica la función de aproximación
     pass
 
-  def get_error_bound(self):
+  @abstractmethod
+  def error_bound(self, r: float, tau: float) -> float:
     # calcula el error de la aproximación
     pass
