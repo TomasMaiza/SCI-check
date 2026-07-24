03ec2bdec5f067753686be1cb763b586fa633e2e
tomasmaiza <tomimaiza@gmail.com>
2026-07-24 10:36:43 -0300

    Agrego verificaciones de dimensiones

--- src/affine_system/affine_mode.py
+++ src/affine_system/affine_mode.py
@@ -5,9 +5,18 @@ import numpy.typing as npt
 @dataclass(frozen=True)
 class AffineMode:
   # Representa un único modo x_dot = A_i*x + b_i
-  def __init__(self, A: npt.NDArray[np.float64], b: npt.NDArray[np.float64]):
-    self.A = A;
-    self.b = b;
+  A: npt.NDArray[np.float64]
+  b: npt.NDArray[np.float64]
+
+  def __post_init__(self):
+    # validamos dimensiones de las matrices
+    if self.A.ndim != 2 or self.A.shape[0] != self.A.shape[1]:
+      raise ValueError(f"La matriz A debe ser cuadrada (n x n)")
+    
+    n = self.A.shape[0]
+        
+    if self.b.shape != (n, 1):
+      raise ValueError(f"El vector b debe ser un vector columna ({n}, 1)")
 
   def get_subsystem(self) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
     return (self.A, self.b)
--- src/affine_system/sas.py
+++ src/affine_system/sas.py
@@ -1,15 +1,25 @@
-from dataclasses import dataclass
 from typing import Dict, List
 import numpy as np
 import numpy.typing as npt
 from .affine_mode import AffineMode
 
-@dataclass(frozen=True)
 class SwitchedAffineSystem:
   # Representa el sistema completo con todos sus modos disponibles.
   def __init__(self, modesDict: Dict[int, (npt.NDArray[np.float64], npt.NDArray[np.float64])]):
+    if not modesDict:
+      raise ValueError("El sistema debe contener al menos un modo.")
+    first = True # para tomar la dimensión del primer modo como referencia
     for i in modesDict.keys():
       A, b = modesDict[i]
+      if first:
+        expectedDimension = A.shape[0]
+        first = False
+      dimension = A.shape[0]
+      if dimension != expectedDimension:
+        raise ValueError(
+                    f"Inconsistencia de dimensiones: El modo {i} tiene "
+                    f"dimensión {dimension}, pero se esperaba {expectedDimension}."
+                )
       self._modes[i] = AffineMode(A, b) # diccionario para mapear un modo a su sistema afín
     
   def get_mode(self, mode: int) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
