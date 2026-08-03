c73436b32b0a0ed2dfac9c20e4863ec0edfb8a4d
tomasmaiza <tomimaiza@gmail.com>
2026-08-03 10:14:37 -0300

    Adapto wrapper para recibir politopo, sistema, dwell time, K y dimension

--- matlab_wrapper.py
+++ matlab_wrapper.py
@@ -4,7 +4,6 @@ import os
 currentDir = os.path.dirname(os.path.abspath(__file__))
 srcPath = os.path.join(currentDir, 'src')
 
-# 2. Le inyectamos esta ruta al cerebro del intérprete de Python de MATLAB
 if srcPath not in sys.path:
     sys.path.insert(0, srcPath)
 
@@ -13,9 +12,9 @@ from sci import SCIChecker
 from geometry import *
 from coverage_checker import *
 import polytope as pc
-from scipy.spatial import ConvexHull
+from affine_system import *
 
-class MatlabWrapper:
+class MatlabWrapperSCI:
   def create_geometry_and_predicates(self, dimension: int):
     # inicializa la geometría y los predicados
     geomClass = GeometryFactory[dimension]
@@ -28,34 +27,25 @@ class MatlabWrapper:
     polytopeVertices = np.array(polytopeVerticesRaw, dtype=float) # vértices del politopo
     self._polytope = pc.qhull(polytopeVertices) # se crea el politopo
 
-  def create_subregions(self, subregionsVerticesRaw: list[list[list[float]]]):
-    # crea la lista de semiespacios que definen cada subregión politópica
-    self._subregions = []
-    for sub in subregionsVerticesRaw:
-      numVertices = len(sub) # sub es una lista de coordenadas [[x1, y1], [x2, y2], ...]
-      if numVertices < 3: # un polígono necesita al menos 3 vértices
-        continue 
-        
-      subArray = np.array(sub, dtype=float) # convertimos la subregión a un array de numpy
-      hull = ConvexHull(subArray) # ordenamos los vértices en sentido antihorario con ConvexHull
-      sortedVertices = subArray[hull.vertices] # LO DE ORDENAR SE PODRÍA HACER CON UN STRATEGY
-        
-      numVertices = len(sortedVertices)
-      subHalfspaces = []
-      for i in range(numVertices): # iteramos para armar los bordes del politopo (v1, v2)
-        v1 = sortedVertices[i]
-        v2 = sortedVertices[(i + 1) % numVertices]
-        p1 = self._geom.create_point(v1)
-        p2 = self._geom.create_point(v2)
-        hs = self._geom.create_halfspace((p1, p2))
-        subHalfspaces.append(hs)
-            
-      self._subregions.append(subHalfspaces) # agregamos la lista de semiespacios de la subregión
+  def create_sas(self, systemRaw: list[tuple[list[list[float]], list[float]]]):
+    # inicializa el sistema afín conmutado
+    modesDict = {}
+    for mode, (ARaw, bRaw) in enumerate(systemRaw):
+      A = np.array(ARaw, dtype=np.float64)
+      b = np.array(bRaw, dtype=np.float64)
+      modesDict[mode] = (A, b)
+    self._sas = SwitchedAffineSystem(modesDict)
 
-  def checking_sci(self, polytopeVerticesRaw: list[list[float]], subregionsVerticesRaw: list[list[list[float]]], dimension: int) -> bool:
+  def checking_sci(self, 
+                   polytopeVerticesRaw: list[list[float]], 
+                   systemRaw: list[tuple[list[list[float]], list[float]]],  # lista de tuplas (A, b)
+                   dwellTime: float, 
+                   K: int, 
+                   dimension: int) -> bool:
     # recibe listas crudas desde MATLAB, arma la geometría y corre el SCIChecker.
     self.create_geometry_and_predicates(dimension)
     self.create_polytope(polytopeVerticesRaw)
-    self.create_subregions(subregionsVerticesRaw)
-    checker = SCIChecker(self._geom, self._preds, self._polytope, self._subregions)
-    return checker.sci_check()
+    self.create_sas(systemRaw)
+    checker = SCIChecker(self._geom, self._preds, self._polytope, self._subregions, self._sas)
+    isSCI, subregions = checker.sci_check(dwellTime, K)
+    return isSCI
