56b991865712384e0ccd6d4f29750c30aa8e1706
tomasmaiza <tomimaiza@gmail.com>
2026-08-10 11:42:23 -0300

    Modifico subregionsStrategy

--- src/polytope_subregions/subregionsStrategy.py
+++ src/polytope_subregions/subregionsStrategy.py
@@ -1,22 +1,21 @@
 from abc import ABC, abstractmethod
 import numpy as np
 import numpy.typing as npt
-import polytope as pc
 from affine_system import *
 from common import PolytopeMap
-from geometry import AbstractHalfspace
+from geometry import AbstractHalfspace, Polytope
 from affine_system import SwitchedAffineSystem
 
 class SubregionsStrategy(ABC):
   @abstractmethod
-  def get_subregion(self, polytope: pc.Polytope) -> list[AbstractHalfspace]:
+  def get_subregion(self, polytope: Polytope) -> list[AbstractHalfspace]:
     # obtiene la subregión para un modo particular
     pass
 
   @abstractmethod
   def get_subregions(self, 
                      sas: SwitchedAffineSystem, 
-                     polytope: pc.Polytope, 
+                     polytope: Polytope, 
                      dwellTime: float, 
                      K: int) -> PolytopeMap:
     # recibe un politopo (y todo lo necesario) para devolver la lista de subregiones
