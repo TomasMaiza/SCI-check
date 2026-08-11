526641d0645b2ad14e0beb5e9f6ff9a4734b20c6
tomasmaiza <tomimaiza@gmail.com>
2026-08-11 11:02:46 -0300

    Arreglo errores de tipado en taylor

--- src/polytope_subregions/approximations/taylor.py
+++ src/polytope_subregions/approximations/taylor.py
@@ -21,33 +21,33 @@ class Taylor(ApproximationStrategy):
     verticesAug = np.hstack((self._S, np.ones((self._S.shape[0], 1))))
     self._R_S = float(np.max(np.linalg.norm(verticesAug, ord=2, axis=1)))
   
-  def apply(self, h: float, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+  def apply(self, h: float, x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]: # type: ignore
     # aplica la función de aproximación
     homx = augmented_x(x)
-    Phi = self.get_matrix(h, self._s, self._M)
+    Phi = self.get_matrix(h)
     return Phi @ homx
 
   def error_bound(self, r: float, tau: float) -> float:
     # calcula el error de la aproximación
-    normA = np.linalg.norm(self._A, ord=2)
+    normA = float(np.linalg.norm(self._A, ord=2))
     M = self._get_M(normA)
     return r * (1 + normA * tau) + M * (np.exp(normA * tau) - 1 - normA * tau)
   
-  def _get_M(self, normA: float):
+  def _get_M(self, normA: float) -> float:
     # auxiliar para calcular M
     if normA < 1e-12:
       return 0.0
     m = -1 # vamos a calcular el valor máximo de una norma así que inicio en un número negativo
     for x in self._S:
       x = x.reshape(-1, 1) # forzamos a x a ser un vector columna
-      norm = np.linalg.norm(self._A @ x + self._b, ord=2)
+      norm = float(np.linalg.norm(self._A @ x + self._b, ord=2))
       if norm > m:
         m = norm
     return 1/normA * m
 
   def error_sequence(self, h: float, K: int) -> list[float]:
     # calcula la secuencia de errores para el método
-    normHomA = np.linalg.norm(self._homA, ord=2)
+    normHomA = float(np.linalg.norm(self._homA, ord=2))
     Cerr = self._get_C_err(normHomA, h)
     errorSeq = []
     for k in range(0, K + 1):
@@ -60,7 +60,7 @@ class Taylor(ApproximationStrategy):
     den = self._s**self._M * factorial(self._M + 1)
     return num / den
   
-  def get_matrix(self, h: float) -> npt.NDArray[np.float64]:
+  def get_matrix(self, h: float) -> npt.NDArray[np.float64]: # type: ignore
     # calcula la matriz de la aproximación tomando el paso s
     tau = h / self._s
     dim = self._homA.shape[0]
