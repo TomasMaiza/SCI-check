06e8b62de064c20bfe16c79c93ba87775f000f0f
tomasmaiza <tomimaiza@gmail.com>
2026-08-14 13:10:32 -0300

    Modifico bindings para incorporar predicados 3d

--- src/bindings/bindings.cpp
+++ src/bindings/bindings.cpp
@@ -8,15 +8,39 @@ PYBIND11_MODULE(pyattene, m) {
 
     py::class_<genericPoint>(m, "GenericPoint"); // Generic Point class
 
+    // structs 2d
     py::class_<explicitPoint2D, genericPoint>(m, "ExplicitPoint2D") // Explicit 2D point class
         .def(py::init<double, double>())
         .def("X", &explicitPoint2D::X)
         .def("Y", &explicitPoint2D::Y);
 
     py::class_<implicitPoint2D_SSI, genericPoint>(m, "ImplicitPoint2D_SSI") // Implicit 2D point class
-        .def(py::init<const explicitPoint2D&, const explicitPoint2D&, const explicitPoint2D&, const explicitPoint2D&>());
+        .def(py::init<const genericPoint&, const genericPoint&, const genericPoint&, const genericPoint&>());
 
-    // orient2d_EEE and orient2d_IEE functions
+    // structs 3d
+    py::class_<explicitPoint3D, genericPoint>(m, "ExplicitPoint3D") 
+        .def(py::init<double, double, double>())
+        .def("X", &explicitPoint3D::X)
+        .def("Y", &explicitPoint3D::Y)
+        .def("Z", &explicitPoint3D::Z);
+
+    // Para C3: Intersección Línea-Plano LPI (Edge vs Face)
+    py::class_<implicitPoint3D_LPI, genericPoint>(m, "ImplicitPoint3D_LPI") 
+        .def(py::init<const genericPoint&, const genericPoint&, 
+                      const genericPoint&, const genericPoint&, const genericPoint&>(),
+             "Interseccion Linea-Plano (LPI). Args: p, q (linea), r, s, t (plano)");
+
+    // Para nueva C4: Intersección de 3 Planos (Face vs Face vs Face)
+    py::class_<implicitPoint3D_TPI, genericPoint>(m, "ImplicitPoint3D_TPI") 
+        .def(py::init<const genericPoint&, const genericPoint&, const genericPoint&, 
+                      const genericPoint&, const genericPoint&, const genericPoint&, 
+                      const genericPoint&, const genericPoint&, const genericPoint&>(),
+             "Interseccion Tres-Planos (TPI). Args: v1,v2,v3 (pl1), w1,w2,w3 (pl2), u1,u2,u3 (pl3)");
+
+
+    // Predicados
     m.def("orient2d_EEE", &orient2d_EEE, "Predicado E-E-E 2D");
     m.def("orient2d_IEE", &orient2d_IEE, "Predicado I-E-E 2D");
+    m.def("orient3d", &genericPoint::orient3D, "Predicado Orient3D dinámico",
+          py::arg("a"), py::arg("b"), py::arg("c"), py::arg("d"));
 }
