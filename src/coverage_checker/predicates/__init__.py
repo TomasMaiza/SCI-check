cc9c41daa099df7fd4b936c57ea3e3252d673aae
tomasmaiza <tomimaiza@gmail.com>
2026-08-18 10:15:01 -0300

    Agrego Predicates2d a PredicatesFactory

--- src/coverage_checker/predicates/__init__.py
+++ src/coverage_checker/predicates/__init__.py
@@ -1,6 +1,7 @@
 from .predicates import AbstractPredicates
 from .predicates_2d import Predicates2d
+from .predicates_3d import Predicates3d
 
-PredicatesFactory = {2: Predicates2d} # agregar los predicados para cada dimensión
+PredicatesFactory = {2: Predicates2d, 3: Predicates3d} # agregar los predicados para cada dimensión
 
 __all__ = ["AbstractPredicates", "PredicatesFactory"] 
