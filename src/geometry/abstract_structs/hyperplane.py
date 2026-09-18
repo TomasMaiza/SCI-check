8f202beef695fdfa3dc69ebc29d102aaed3610c4
tomasmaiza <tomimaiza@gmail.com>
2026-09-18 12:16:17 -0300

    Correcciones en inicialización de hiperplanos

--- src/geometry/abstract_structs/hyperplane.py
+++ src/geometry/abstract_structs/hyperplane.py
@@ -6,10 +6,10 @@ from fractions import Fraction
 
 class Hyperplane:
   # representación de un hiperplano  
-  def __init__(self, points: list[AbstractPoint], intDim: int, ambDim: int):
+  def __init__(self, points: list[float]):
         # 2 puntos para un hiperplano (recta) si la dimensión ambiental es 2
         # 3 puntos para un hiperplano (plano) si la dimensión ambiental es 3
     self._points = points
 
-  def get_points(self) -> tuple[AbstractPoint, ...]:
+  def get_points(self) -> list[float]:
     return self._points
