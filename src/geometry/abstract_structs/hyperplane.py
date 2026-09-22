a48260f73e6bf31b60989f2ff8a5f541debdc92c
tomasmaiza <tomimaiza@gmail.com>
2026-09-22 10:04:52 -0300

    Agrego tipo Point a Hyperplane

--- src/geometry/abstract_structs/hyperplane.py
+++ src/geometry/abstract_structs/hyperplane.py
@@ -1,13 +1,17 @@
 import numpy as np
 from geometry.abstract_structs.point import AbstractPoint
 from fractions import Fraction
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+  from src.common.types import Point
 
 class Hyperplane:
   # representación de un hiperplano  
-  def __init__(self, points: list[tuple[float, float]]):
+  def __init__(self, points: list['Point']):
         # 2 puntos para un hiperplano (recta) si la dimensión ambiental es 2
         # 3 puntos para un hiperplano (plano) si la dimensión ambiental es 3
     self._points = points
 
-  def get_points(self) -> list[tuple[float, float]]:
+  def get_points(self) -> list['Point']:
     return self._points
